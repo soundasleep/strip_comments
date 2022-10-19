23938a806b5f8f3c0f0eabc61c08baf320282ba9
Jevon Wright <jevon@jevon.org>
2022-10-20 09:26:14 +1300

    Initial implementation of YAML strip

--- lib/strip_comments.rb
+++ lib/strip_comments.rb
@@ -4,5 +4,17 @@ require_relative "strip_comments/version"
 
 module StripComments
   class Error < StandardError; end
-  # Your code goes here...
+
+  def self.strip(language, str) 
+    case language
+    when "yaml", "yml"
+      strip_yaml(str)
+    else
+      raise Error.new("unknown language '#{language}'")
+    end
+  end
+
+  def self.strip_yaml(str)
+    str.gsub(/#[^\n]+/, '')
+  end
 end
--- spec/strip_comments_spec.rb
+++ spec/strip_comments_spec.rb
@@ -1,11 +1,32 @@
 # frozen_string_literal: true
 
+require "pathname"
+
 RSpec.describe StripComments do
   it "has a version number" do
     expect(StripComments::VERSION).not_to be nil
   end
 
-  it "does something useful" do
-    expect(false).to eq(true)
+  Dir.glob(File.join(File.dirname(__FILE__), "tests", "*")).each do |filename|
+    language = Pathname.new(filename).split.last.to_s
+    extension = language
+
+    describe "#{language}" do
+      Dir.glob(File.join(filename, "*")).reject { |f| f.include?(".expected") }.each do |test|
+        test_filename = Pathname.new(test).split.last.to_s
+
+        describe "#{test_filename}" do
+          expected = test.gsub(/(.*)\./, '\\1.expected.')
+          expected_filename = Pathname.new(expected).split.last.to_s
+
+          it "is stripped to #{expected_filename}" do
+            input = File.open(test).read
+            goal = File.open(expected).read
+
+            expect(StripComments::strip(language, input)).to eq goal
+          end
+        end
+      end
+    end
   end
 end
